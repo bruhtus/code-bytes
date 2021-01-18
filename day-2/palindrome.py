ff0264ca9417bb665b87ae721946450866795693
bruhtus <hibruhtus@gmail.com>
2021-01-18 13:26:12 +0700

    Using string CLI input instead of list

--- day-2/palindrome.py
+++ day-2/palindrome.py
@@ -1,5 +1,14 @@
-string = ['level', 'algorithm', 'A man, a plan, a canal: Panama']
+from fire import Fire
 
-for s in string:
-    s_without_symbols = s.replace(',', '').replace(':', '').replace(' ', '')
-    print(f'{s} -> true') if s_without_symbols.lower() == s_without_symbols.lower()[::-1] else print(f'{s} -> false')
+def main(input):
+    #string = ['level', 'algorithm', 'A man, a plan, a canal: Panama.']
+
+    #for s in string:
+        #s_without_symbols = s.replace(',', '').replace(':', '').replace(' ', '')
+        #print(f'{s} -> true') if s_without_symbols.lower() == s_without_symbols.lower()[::-1] else print(f'{s} -> false')
+
+    input_without_symbols = input.replace(',', '').replace(':', '').replace(' ', '').replace('.', '')
+    print(f'{input} -> true') if input_without_symbols.lower() == input_without_symbols.lower()[::-1] else print(f'{input} -> false')
+
+if __name__ == '__main__':
+    Fire(main)
