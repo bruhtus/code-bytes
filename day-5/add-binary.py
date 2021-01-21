776c57b8ce02fb304d74e4add8ee57446411fd66
bruhtus <hibruhtus@gmail.com>
2021-01-21 10:59:02 +0700

    Add solution for challenge day 5

---
+++ day-5/add-binary.py
@@ -0,0 +1,10 @@
+from fire import Fire
+
+def main(string1, string2):
+    a = str(string1)
+    b = str(string2)
+    s = bin(int(a, 2) + int(b, 2))
+    print(s[2::])
+
+if __name__ == '__main__':
+    Fire(main)
