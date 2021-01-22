ec215a48648744915f9da88e2d12d029fa21c723
bruhtus <hibruhtus@gmail.com>
2021-01-22 11:05:14 +0700

    Update reverse string using fire python module

--- day-1/reverse-string.py
+++ day-1/reverse-string.py
@@ -1,6 +1,12 @@
-f = open('input', 'r')
+from fire import Fire
 
-string = f.readlines()
+def main(file_input):
+    f = open(file_input, 'r')
 
-for s in string:
-    print(s[::-1])
+    string = f.readlines()
+
+    for s in string:
+        print(s[::-1].strip())
+
+if __name__ == '__main__':
+    Fire(main)
