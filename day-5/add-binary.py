92293af08540038ea8373035cb488c4e627d78f1
bruhtus <hibruhtus@gmail.com>
2021-01-22 14:51:17 +0700

    Remove unnecessary stuff

--- day-5/add-binary.py
+++ day-5/add-binary.py
@@ -4,7 +4,7 @@ def main(string1, string2):
     a = str(string1)
     b = str(string2)
     s = bin(int(a, 2) + int(b, 2))
-    print(s[2::])
+    print(s[2:])
 
 if __name__ == '__main__':
     Fire(main)
